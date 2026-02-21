21234201594feffc2f89ac17e66163927ea00bfe
Osmar Montesinos <osmarmontes50@gmail.com>
2026-02-20 22:41:04 -0500

    avance

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,24 @@
+import { useRef } from 'react';
+
 const Register = () => {
+
+
+
   return (
-    <div>Register</div>
+    <div className='mx-auto w-96 mt-24 border p-6 rounded-lg shadow-md'>
+      <h1 className='text-2xl font-bold mb-4'>Register</h1>
+      <form className='space-y-4'>
+        <div>
+          <label className='block text-md font-medium mb-1' htmlFor="email">Email</label>
+          <input className="input w-full" type="email" id="email" name="email" required />
+        </div>
+        <div>
+          <label className='block text-md font-medium mb-1' htmlFor="password">Password</label>
+          <input className="input w-full" type="password" id="password" name="password" required />
+        </div>
+        <button className='btn btn-primary w-full' type="submit">Register</button>
+      </form>
+    </div>
   )
 }
 
