a226b5c7d48c1e888544794cf840f82d57fdc474
Osmar Montesinos <osmarmontes50@gmail.com>
2026-02-20 20:25:25 -0500

    avance

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,6 @@
 import { useProducts } from '../modules/products/hooks/useProducts';
 import { ProductList } from '../modules/products/components/ProductList';
+import useGetAxios from '../hooks/useAxios';
 
 export const Products = () => {
   const { products, filter, setFilter, categories } = useProducts();
@@ -12,10 +13,15 @@ export const Products = () => {
     setFilter({ ...filter, searchText: e.target.value });
   };
 
+  const URL = "https://minimal-product-api.onrender.com/api/products";
+
+  const { data } = useGetAxios(URL);
+  console.table(data);
+
   return (
     <div>
       <h1 className="text-3xl font-bold mb-6">Catálogo de Productos</h1>
-      
+
       <div className="flex flex-col md:flex-row gap-4 mb-8">
         <div className="form-control w-full md:w-auto">
           <input
@@ -26,7 +32,7 @@ export const Products = () => {
             onChange={handleSearchChange}
           />
         </div>
-        
+
         <div className="form-control w-full md:w-auto">
           <select
             className="select select-bordered w-full md:w-64"
