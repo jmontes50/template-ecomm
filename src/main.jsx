d71e0c9e8f6177f9399a68875f4379c1e0ad402d
Osmar Montesinos <osmarmontes50@gmail.com>
2026-02-26 22:36:40 -0500

    avance

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import "leaflet/dist/leaflet.css";
+import "leaflet-control-geocoder/dist/Control.Geocoder.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
