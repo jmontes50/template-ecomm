4196939ca52704d6f8210541c5b4355369af1167
Osmar Montesinos <osmarmontes50@gmail.com>
2026-02-27 19:57:43 -0500

    avance

--- src/config/configFirebase.js
+++ src/config/configFirebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,10 +20,13 @@ const app = initializeApp(firebaseConfig);
 // Referencia al módulo de autenticación de Firebase
 const auth = getAuth(app);
 
+const db = getFirestore(app);
+
 export default app;
 
 export {
-  auth
+  auth,
+  db
 }
 
 
